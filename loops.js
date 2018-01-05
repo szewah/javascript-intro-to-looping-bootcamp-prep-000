dcf704d63f8744630ba6964e5130e2d73a39fd69
Szewah Chin <swgrleong@gmail.com>
2018-01-05 16:06:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,10 +22,10 @@ function doWhileLoop(array) {
   }
 }
  
-// do {
-//   array.pop()
-// } while (array.length > 0 && maybeTrue())
-//   return array
-// }
+do {
+  array.pop()
+} while (array.length > 0 && maybeTrue())
+  return array
+}
 
 
